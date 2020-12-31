6fa385b65d5c1984f9ff69ee7f2a6f88ac56f6a1
Federico-Aguirre <56941776+Federico-Aguirre@users.noreply.github.com>
2020-12-30 23:53:29 -0300

    initial commit

---
+++ php-mysql-crud-master/includes/header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <title>PHP CRUD MYSQL</title>
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <!-- BOOTSTRAP 4 -->
+    <link rel="stylesheet" href="https://bootswatch.com/4/yeti/bootstrap.min.css">
+    <!-- FONT AWESOEM -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css">
+  </head>
+  <body>
+
+    <nav class="navbar navbar-dark bg-dark">
+      <div class="container">
+        <a class="navbar-brand" href="index.php">PHP MySQL CRUD</a>
+      </div>
+    </nav>
